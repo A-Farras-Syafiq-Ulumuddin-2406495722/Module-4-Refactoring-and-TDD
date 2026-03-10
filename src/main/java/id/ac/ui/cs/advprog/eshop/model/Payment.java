fc79d36b47f0b7cd51a816dc3cd23e96ccd85d1b
Rainmo0 <aunknown145@gmail.com>
2026-03-10 20:11:21 +0700

    [RED] refactor due to mistake

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -7,20 +7,4 @@ public class Payment {
     String method;
     String status;
     Map<String, String> paymentData;
-
-    public Payment addPayment(Order order, String method, Map<String, String> paymentData) {
-        return null;
-    }
-
-    public Payment setStatus(Payment payment, String status) {
-        return null;
-    }
-
-    public Payment getPayment(String paymentId) {
-        return null;
-    }
-
-    public Payment getAllPayments() {
-        return null;
-    }
 }
